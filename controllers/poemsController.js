8d48288506440ca2cc53eefbd30b4a6df34934b4
ramprasathmk <mkramprasath12@gmail.com>
2024-11-26 23:50:50 +0545

    fix: simplified error handling approach
    Signed-off-by: ramprasathmk <mkramprasath12@gmail.com>

--- controllers/poemsController.js
+++ controllers/poemsController.js
@@ -1,37 +1,44 @@
 const Poem = require('../models/poem');
 
-exports.getAllPoems = async (req, res) => {
-    const poems = await Poem.find();
-    res.render('index', { poems });
+// Error handling middleware wrapper
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next);
+    };
 };
 
-exports.createPoem = async (req, res) => {
+exports.getAllPoems = asyncHandler(async (req, res) => {
+    const poems = await Poem.find();
+    res.render('index', { poems });
+});
+
+exports.createPoem = asyncHandler(async (req, res) => {
     const { title, body, author } = req.body;
     await Poem.create({ title, body, author });
     res.redirect('/poems');
-};
+});
 
-exports.getEditPoem = async (req, res) => {
+exports.getEditPoem = asyncHandler(async (req, res) => {
     const poem = await Poem.findById(req.params.id);
-    res.render('edit', { poem });
-};
-
-exports.updatePoem = async (req, res) => {
-    const { title, body, author } = req.body;
-    await Poem.findByIdAndUpdate(req.params.id, { title, body, author });
-    res.redirect('/poems');
-};
-
-exports.deletePoem = async (req, res) => {
-    await Poem.findByIdAndDelete(req.params.id);
-    res.redirect('/poems');
-};
-
-exports.getEditPoem = async (req, res) => {
-    try {
-        const poem = await Poem.findById(req.params.id);
-        res.render('edit', { poem });
-    } catch (err) {
-        res.status(500).send(err);
+    if (!poem) {
+        return res.status(404).send('Poem not found');
     }
-};
+    res.render('edit', { poem });
+});
+
+exports.updatePoem = asyncHandler(async (req, res) => {
+    const { title, body, author } = req.body;
+    const poem = await Poem.findByIdAndUpdate(req.params.id, { title, body, author }, { new: true });
+    if (!poem) {
+        return res.status(404).send('Poem not found');
+    }
+    res.redirect('/poems');
+});
+
+exports.deletePoem = asyncHandler(async (req, res) => {
+    const poem = await Poem.findByIdAndDelete(req.params.id);
+    if (!poem) {
+        return res.status(404).send('Poem not found');
+    }
+    res.redirect('/poems');
+});
